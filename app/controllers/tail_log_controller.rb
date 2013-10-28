ccc1ec69d3bdbc46d88be355dfceba006896c0f5
Ryan Mulligan <rmulligan@yottamark.com>
2013-10-28 13:58:53 -0700

    Added Ansicolor translator

--- app/controllers/tail_log_controller.rb
+++ app/controllers/tail_log_controller.rb
@@ -1,10 +1,12 @@
 class TailLogController < ApplicationController
+  require 'term/ansicolor'
+  include Term::ANSICOLOR
   def index
-  	@lines = params[:lines] ||= 100
-	@stage = (params[:stage] ||= "development") + ".log"
-  	@file = "#{Rails.root}/log/#{@stage}"
-	@command = "tail -n #{@lines} #{@file}"
-	@output = `#{@command}`
+    @lines = params[:lines] ||= 100
+  @stage = (params[:stage] ||= "development") + ".log"
+    @file = "#{Rails.root}/log/#{@stage}"
+  @command = "tail -n #{@lines} #{@file}"
+  @output = `#{@command}`
     render :text => "<code>#{@output.gsub("\n","<br/>")} </code>"
   end
 
